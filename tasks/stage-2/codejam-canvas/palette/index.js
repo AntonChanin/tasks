7569d33431523d8dbe90ca79f709ecf45a81a39e
antonvh <antonhaninvl@gmail.com>
2019-11-15 16:30:00 +0300

    fix: some errors fix. exept 5,7,8 as did not understand that it is necessary

--- tasks/stage-2/codejam-canvas/palette/index.js
+++ tasks/stage-2/codejam-canvas/palette/index.js
@@ -11,6 +11,8 @@ const medium = matrixs.querySelector('#medium');
 const big = matrixs.querySelector('#big');
 const redChoose = colors.querySelector('#red');
 const blueChoose = colors.querySelector('#blue');
+const ref4x = 'https://raw.githubusercontent.com/AntonChanin/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json';
+const ref32X = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/32x32.json';
 
 var currentColor = 'green';
 var prevColor = undefined;
@@ -22,6 +24,21 @@ var chooseToolUse = false;
 workSpace.addEventListener('mousedown', down);
 workSpace.addEventListener('mouseup', toggledraw);
 currentColorTool.style.background = currentColor;
+
+var getJSON = function (url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        var status = xhr.status;
+        if (status == 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status);
+        }
+    };
+    xhr.send();
+};
 
 let idt = localStorage.getItem('imgData') || null;
 if (idt !== null) {
@@ -35,40 +52,23 @@ if (idt !== null) {
 
 
 small.addEventListener('click', function () {
-    let ref = 'https://raw.githubusercontent.com/AntonChanin/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json';
-    var getJSON = function (url, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'json';
-        xhr.onload = function () {
-            var status = xhr.status;
-            if (status == 200) {
-                callback(null, xhr.response);
-            } else {
-                callback(status);
-            }
-        };
-        xhr.send();
-    };
-    getJSON(ref,
+    getJSON(ref4x,
     function (err, data) {
         if (err != null) {
             alert('Something went wrong: ' + err);
         } else {
+            let size = 1;
             if (sizeInput.value == '') {
-                sizeInput.value = 1;
+                size = 1;
+            } else {
+                size = sizeInput.value 
             }
-            if (sizeInput.value > 2) {
-                sizeInput.value = 2;
-            }
-            var canvas = document.querySelector("canvas"), // Select our canvas element
-            ctx = canvas.getContext("2d"), // Save the context we're going to use
-            width = data[0].length, // Get the width of the array
+        let width = data[0].length, // Get the width of the array
             height = data.length, // Get the height of the array
-            scale = 10 * sizeInput.value; // Scales the whole image by this amount, set to 1 for default size
+            scale = 10 * size; // Scales the whole image by this amount, set to 1 for default size
             // Make sure the canvas is no larger than the size we need
-            canvas.width = width * scale;
-            canvas.height = height * scale;
+            workSpace.width = width * scale;
+            workSpace.height = height * scale;
             // Loop through each color and draw that section
             for (let row = 0; row < height; row++) {
                 for (let col = 0; col < width; col++) { // Since there are nested arrays we need two for loops
@@ -82,40 +82,23 @@ small.addEventListener('click', function () {
 });
 
 medium.addEventListener('click', function () {
-    let ref = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/32x32.json';
-    var getJSON = function (url, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'json';
-        xhr.onload = function () {
-            var status = xhr.status;
-            if (status == 200) {
-                callback(null, xhr.response);
-            } else {
-                callback(status);
-            }
-        };
-        xhr.send();
-    };
-    getJSON(ref,
+    getJSON(ref32X,
     function (err, data) {
         if (err != null) {
             alert('Something went wrong: ' + err);
         } else {
+            let size = 1;
             if (sizeInput.value == '') {
-                sizeInput.value = 1;
+                size = 1;
+            } else {
+                size = sizeInput.value
             }
-            if (sizeInput.value > 2) {
-                sizeInput.value = 2;
-            }
-            var canvas = document.querySelector("canvas"), // Select our canvas element
-            ctx = canvas.getContext("2d"), // Save the context we're going to use
             width = data[0].length, // Get the width of the array
             height = data.length, // Get the height of the array
-            scale = 10 * sizeInput.value; // Scales the whole image by this amount, set to 1 for default size
+            scale = 10 * size; // Scales the whole image by this amount, set to 1 for default size
             // Make sure the canvas is no larger than the size we need
-            canvas.width = width * scale;
-            canvas.height = height * scale;
+            workSpace.width = width * scale;
+            workSpace.height = height * scale;
 
             // Loop through each color and draw that section
             for (let row = 0; row < height; row++) {
@@ -132,20 +115,18 @@ medium.addEventListener('click', function () {
 });
 
 big.addEventListener('click', function () {
+    let size = 1;
     if (sizeInput.value == '') {
-        sizeInput.value = 1;
+        size = 1;
+    } else {
+        size = sizeInput.value
     }
-    if (sizeInput.value > 2) {
-        sizeInput.value = 2;
-    }
-    var canvas = document.querySelector("canvas"), // Select our canvas element
-        ctx = canvas.getContext("2d"); // Save the context we're going to use
-    width = 256, // Get the width of the array
-    height = 256, // Get the height of the array
-    scale = 1 * sizeInput.value; // Scales the whole image by this amount, set to 1 for default size
+    let width = 256, // Get the width of the array
+        height = 256, // Get the height of the array
+        scale = 1 * size; // Scales the whole image by this amount, set to 1 for default size
     // Make sure the canvas is no larger than the size we need
-    canvas.width = width * scale;
-    canvas.height = height * scale;
+    workSpace.width = width * scale;
+    workSpace.height = height * scale;
     drawing = new Image();
     drawing.src = "./data/image.png"; // can also be a remote URL e.g. http://
     drawing.onload = function () {
@@ -171,14 +152,12 @@ function getMousePos(canvas, evt) {
 }
 
 function draw(canvas, posx, posy) {
-    let context = canvas.getContext('2d');
     if (md) {
-        context.fillRect(posx, posy, 4, 4);
+        ctx.fillRect(posx, posy, 4, 4);
     }
 }
 
 function chooseColor(context) {
-    let ctx = workSpace.getContext("2d");
     if (context == redChoose) {
         prevColor = currentColor;
         currentColor = 'red';
